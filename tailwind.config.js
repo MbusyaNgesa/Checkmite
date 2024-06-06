8c4ca48a6234b3768ed4f457b2bc7a9adaf15c67
MbusyaNgesa <theengesa@gmail.com>
2024-06-06 16:36:50 +0300

    Navbar Section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      yellow: "#c5630c",
+      purple: "#A47F6F",
+      grey: "#415057",
+      darkgrey: "#33353A",
+      black: "#000000",
+      charcoal: "#1A1F24",
+      white: "#fff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
