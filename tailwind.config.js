ff7ae81ecf4853952f1ae27f181d623584acbaa1
MbusyaNgesa <theengesa@gmail.com>
2024-06-08 20:42:31 +0300

    Categories section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,6 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    colors: {
-      yellow: "#c5630c",
-      purple: "#A47F6F",
-      grey: "#415057",
-      darkgrey: "#33353A",
-      black: "#000000",
-      charcoal: "#1A1F24",
-      white: "#fff",
-    },
     extend: {},
   },
   plugins: [],
